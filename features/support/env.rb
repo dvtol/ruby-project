81c9576db70e026a655708c0becf36234f6e798e
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-19 23:51:51 +0100

    added remote phantomjs to env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,28 +3,29 @@ require 'rspec'
 require 'selenium/webdriver'
 require 'cucumber/rspec/doubles'
 
-#url = "http://otwn.nl:4444/wd/hub"
+# added grid hub for FreeBSD (remote testing)
+url = "http://otwn.nl:4444/wd/hub"
 
-#capabilities = Selenium::WebDriver::Remote::Capabilities.new
+capabilities = Selenium::WebDriver::Remote::Capabilities.new
 
-#capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
-#capabilities['browserName'] = ENV['SELENIUM_BROWSER'] || 'chrome'
+capabilities['platform'] = ENV['PLATFORM'] || 'ANY'
+capabilities['browserName'] = ENV['BROWSER'] || 'phantomjs'
 
-#browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
 
-#at_exit do
-#  browser.quit
-#end
-
-# added Chrome for Windows (local testing)
-Capybara.register_driver :chrome do |app|
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome()
-
-  Capybara::Selenium::Driver.new(app, {:browser => :chrome, :desired_capabilities => caps})
+at_exit do
+  browser.quit
 end
 
-Capybara.default_driver = case ENV['BROWSER']
-                            when 'chrome'
-                              :chrome
-                          end
+
+# added Chrome for Windows (local testing)
+#Capybara.register_driver :chrome do |app|
+#  caps = Selenium::WebDriver::Remote::Capabilities.chrome()
+#  Capybara::Selenium::Driver.new(app, {:browser => :chrome, :desired_capabilities => caps})
+#end
+
+#Capybara.default_driver = case ENV['BROWSER']
+#                            when 'chrome'
+#                              :chrome
+#                          end
 
