320030de4442d35e9c4229ee0382f72763e66ef8
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-19 23:13:43 +0100

    modified env.rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,6 @@ require 'selenium/webdriver'
 url = "http://otwn.nl:4444/wd/hub"
 
 capabilities = Selenium::WebDriver::Remote::Capabilities.new
-Selenium::WebDriver::Chrome::Service.executable_path = '/usr/local/bin/chromedriver'
 
 capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
 capabilities['browserName'] = ENV['SELENIUM_BROWSER'] || 'chrome'
