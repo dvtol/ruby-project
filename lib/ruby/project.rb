7a97f73e75ee3420fe7cabff79c3131eb463afa4
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-19 20:39:50 +0100

    deleted files

--- lib/ruby/project.rb
+++
@@ -1,7 +0,0 @@
-require "ruby/project/version"
-
-module Ruby
-  module Project
-    # Your code goes here...
-  end
-end
--- version.rb
+++
@@ -1,5 +0,0 @@
-module Ruby
-  module Project
-    VERSION = "0.1.0"
-  end
-end
