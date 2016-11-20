ccb5d32e6944ce23cc9731f4f2908a439a375795
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-20 09:36:33 +0100

    removed the fail statement to continue the run

--- features/step_definitions/sample.rb
+++ features/step_definitions/sample.rb
@@ -2,10 +2,9 @@
 Given /^startup the framework and visit otwn website/ do
 	
   visit('http://otwn.nl/')
-  sleep(4)
+  sleep(3)
     page.save_screenshot('sample-otwn.png')
-  sleep(4)
-    fail
+  sleep(3)
     page.driver.quit
 end
 
