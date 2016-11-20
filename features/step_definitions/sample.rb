863b29ff380988d147af7155e5c0e4478f6986d6
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-20 08:25:25 +0100

    changed feature file and sample ruby file

--- features/step_definitions/sample.rb
+++ features/step_definitions/sample.rb
@@ -2,6 +2,8 @@
 Given /^startup the framework and visit otwn website/ do
 	
   visit('http://otwn.nl/')
-  sleep(6)
+  sleep(4)
+    page.save_screenshot('sample-otwn.png')
+  sleep(4)
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,28 +4,27 @@ require 'selenium/webdriver'
 require 'cucumber/rspec/doubles'
 
 # added grid hub for FreeBSD (remote testing)
-url = "http://otwn.nl:4444/wd/hub"
+#url = "http://otwn.nl:4444/wd/hub"
 
-capabilities = Selenium::WebDriver::Remote::Capabilities.new
+#capabilities = Selenium::WebDriver::Remote::Capabilities.new
 
-capabilities['platform'] = ENV['PLATFORM'] || 'ANY'
-capabilities['browserName'] = ENV['BROWSER'] || 'phantomjs'
+#capabilities['platform'] = ENV['PLATFORM'] || 'ANY'
+#capabilities['browserName'] = ENV['BROWSER'] || 'phantomjs'
 
-browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+#browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
 
-at_exit do
-  browser.quit
-end
-
-
-# added Chrome for Windows (local testing)
-#Capybara.register_driver :chrome do |app|
-#  caps = Selenium::WebDriver::Remote::Capabilities.chrome()
-#  Capybara::Selenium::Driver.new(app, {:browser => :chrome, :desired_capabilities => caps})
+#at_exit do
+#  browser.quit
 #end
 
-#Capybara.default_driver = case ENV['BROWSER']
-#                            when 'chrome'
-#                              :chrome
-#                          end
+# added Chrome for Windows (local testing)
+Capybara.register_driver :phantomjs do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.phantomjs()
+  Capybara::Selenium::Driver.new(app, {:browser => :phantomjs, :desired_capabilities => caps})
+end
+
+Capybara.default_driver = case ENV['BROWSER']
+                            when 'phantomjs'
+                              :phantomjs
+                          end
 
