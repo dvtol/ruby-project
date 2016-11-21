5808007234b68b12340ccef5b411bd9cee1077fc
Danny van Tol <danny.van.tol@ordina.nl>
2016-11-21 16:01:52 +0100

    added fail

--- features/step_definitions/sample.rb
+++ features/step_definitions/sample.rb
@@ -2,7 +2,7 @@
 Given /^@startup the framework and visit otwn website/ do
 
   # open otwn website
-  visit('http://otwn.nl/')
+  visit('http://otwn.nl/')asdasd
   sleep(2)
     page.save_screenshot('sample-otwn1.png')
   sleep(2)
